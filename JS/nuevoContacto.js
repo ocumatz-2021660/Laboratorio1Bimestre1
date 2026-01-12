9b1e99a067ee7c3b977ed85d2b875417cf740f55
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-01-12 10:00:52 -0600

    agregando estilo Java Scrip en Nuevo contacto (ocultar y mostrar)

---
+++ JS/nuevoContacto.js
@@ -0,0 +1,7 @@
+function abrirNuevoContacto(){
+    document.getElementById("pestaña-emergente").style.display = "flex";
+}
+
+function cerrarNuevoContacto(){
+    document.getElementById("pestaña-emergente").style.display = "none";
+}
