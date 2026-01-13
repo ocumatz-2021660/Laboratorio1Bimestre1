52635599627689b5965bdefdf9529eada1fb6c11
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-01-12 18:13:51 -0600

    agregando funcionalidad Detalle contacto con java script

--- JS/nuevoContacto.js
+++ JS/nuevoContacto.js
@@ -1,7 +1,19 @@
-function abrirNuevoContacto(){
+function abrirNuevoContacto() {
     document.getElementById("pestaña-emergente").style.display = "flex";
 }
 
-function cerrarNuevoContacto(){
+function cerrarNuevoContacto() {
     document.getElementById("pestaña-emergente").style.display = "none";
+}
+
+function abrirDetalleContacto() {
+    const ventanaEmergente = parent.document.getElementById("pestana-detalle");
+
+    if (ventanaEmergente) {
+        ventanaEmergente.style.display = "flex";
+    }
+}
+
+function cerrarDetalleContacto() {
+    document.getElementById("pestana-detalle").style.display = "none";
 }
