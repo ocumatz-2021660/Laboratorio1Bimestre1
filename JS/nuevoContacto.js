0b9e3b3ffc79a84dcb85f1eb7e9ab556137e6e3a
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-01-12 22:24:37 -0600

    agregando funcionalidad favoritos con java Script

--- JS/nuevoContacto.js
+++ JS/nuevoContacto.js
@@ -1,3 +1,4 @@
+/*Nuevo contactos*/ 
 function abrirNuevoContacto() {
     document.getElementById("pestaña-emergente").style.display = "flex";
 }
@@ -6,6 +7,7 @@ function cerrarNuevoContacto() {
     document.getElementById("pestaña-emergente").style.display = "none";
 }
 
+/*Detalles contacto*/ 
 function abrirDetalleContacto() {
     const ventanaEmergente = parent.document.getElementById("pestana-detalle");
 
@@ -16,4 +18,43 @@ function abrirDetalleContacto() {
 
 function cerrarDetalleContacto() {
     document.getElementById("pestana-detalle").style.display = "none";
+}
+
+/* Favoritos */
+
+function filtrarFavoritos() {
+    const filas = document.querySelectorAll('.fila-contacto');
+
+    filas.forEach(fila => {
+        const checkbox = fila.querySelector('input[type="checkbox"]');
+        
+        if (checkbox.checked) {
+            fila.style.display = ""; 
+        } else {
+            fila.style.display = "none"; 
+        }
+    });
+}
+
+function mostrarSoloFavoritos() {
+    const iframe = document.querySelector('.vista-frame');
+    if (iframe && iframe.contentWindow.filtrarFavoritos) {
+        iframe.contentWindow.filtrarFavoritos();
+    }
+}
+
+function mostrarTodosLosContactos() {
+    const filas = document.querySelectorAll('.fila-contacto');
+
+    filas.forEach(fila => {
+        fila.style.display = ""; 
+    });
+}
+
+function verInicio() {
+    const iframe = document.querySelector('.vista-frame');
+
+    if (iframe && iframe.contentWindow.mostrarTodosLosContactos) {
+        iframe.contentWindow.mostrarTodosLosContactos();
+    }
 }
